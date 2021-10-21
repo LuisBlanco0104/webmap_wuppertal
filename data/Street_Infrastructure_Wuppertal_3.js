44c593e6511092e09f6be61fdf7dd2ec953edeaa
Luis Blanco <74207671+LuisBlanco0104@users.noreply.github.com>
2021-10-21 13:03:28 +0200

    adding files from QGIS2web

---
+++ data/Street_Infrastructure_Wuppertal_3.js
@@ -0,0 +1,1 @@
+var json_Street_Infrastructure_Wuppertal_3 = {"type":"FeatureCollection","name":"Street_Infrastructure_Wuppertal_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
